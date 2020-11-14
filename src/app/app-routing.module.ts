e5b9f77a7c79e18ed6e914370552dfaee86615f9
Mugisha Thierry <thierrymugis@gmail.com>
2020-11-15 00:36:10 +0200

    Added routing path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { RepoComponent } from './repo/repo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: UserComponent},
+  { path: 'repositories', component: RepoComponent},
+  { path: '', redirectTo: "/home", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
