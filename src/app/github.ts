503de7a65eb48e40f00e2bb7ba1ad5644086661f
Mugisha Thierry <thierrymugis@gmail.com>
2020-11-15 00:47:24 +0200

    Created github class

---
+++ src/app/github.ts
@@ -0,0 +1,3 @@
+export class Github {
+    constructor(public avatar_url:string,public name:string,public login:string, public public_repos:number, public followers:number, public following:number,public created_at:Date ){}
+}
